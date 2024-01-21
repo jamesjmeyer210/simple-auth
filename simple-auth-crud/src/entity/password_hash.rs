1c7da8a411b11863d132bb4aa398e505b7bbadab
James Meyer <jamesjmeyer210@gmail.com>
2024-01-21 15:48:30 -0700

    Add salt to password

--- simple-auth-crud/src/entity/password_hash.rs
+++ simple-auth-crud/src/entity/password_hash.rs
@@ -10,14 +10,29 @@ use sqlx::{Database, Decode, Type};
 use sqlx::database::{HasValueRef};
 use sqlx::error::BoxDynError;
 
+const SALT_LEN: usize = 16;
+const HASH_LEN: usize = 32;
+
 pub(crate) struct PasswordHash {
-    _inner: [u8;32]
+    salt: [u8;SALT_LEN],
+    hash: [u8;HASH_LEN]
 }
 
-// TODO: Implement explicit decode
 impl PasswordHash {
-    pub fn as_bytes(&self) -> &[u8] {
-        self._inner.as_ref()
+    pub fn to_vec(&self) -> Vec<u8> {
+        let mut vec = Vec::with_capacity(SALT_LEN + HASH_LEN);
+        let mut salt = self.salt.to_vec();
+        let mut hash = self.hash.to_vec();
+        vec.append(&mut salt);
+        vec.append(&mut hash);
+        vec
+    }
+
+    pub fn verify(&self, password: &Password) -> Result<bool,argon2::Error> {
+        let argon2 = Argon2::default();
+        let mut hash = [0u8;HASH_LEN];
+        argon2.hash_password_into(password.as_bytes(), &self.salt, &mut hash)?;
+        Ok(hash == self.hash)
     }
 }
 
@@ -26,13 +41,15 @@ impl TryFrom<&Password> for PasswordHash {
 
     fn try_from(value: &Password) -> Result<Self, Self::Error> {
         let argon2 = Argon2::default();
-        let mut salt = [0u8; 16];
+        let mut salt = [0u8; SALT_LEN];
         OsRng.fill_bytes(&mut salt);
 
-        let mut bytes = [0u8; 32];
-        argon2.hash_password_into(value.as_bytes(), &salt, &mut bytes)?;
+        let mut hash = [0u8; HASH_LEN];
+        argon2.hash_password_into(value.as_bytes(), &salt, &mut hash)?;
+
         Ok(PasswordHash {
-            _inner: bytes
+            salt,
+            hash
         })
     }
 }
@@ -42,8 +59,11 @@ impl<'r, DB: Database> Decode<'r, DB> for PasswordHash where &'r [u8]: Decode<'r
     fn decode(value: <DB as HasValueRef<'r>>::ValueRef) -> Result<Self, BoxDynError> {
         let value = <&[u8] as Decode<DB>>::decode(value)?;
 
+        let split = value.split_at(SALT_LEN);
+
         Ok(PasswordHash {
-            _inner: <[u8;32]>::try_from(value)?
+            salt: <[u8;SALT_LEN]>::try_from(split.0)?,
+            hash: <[u8;HASH_LEN]>::try_from(split.1)?
         })
     }
 }
@@ -71,5 +91,28 @@ mod test {
         let p = p.unwrap();
         let h = PasswordHash::try_from(&p);
         assert!(h.is_ok())
+    }
+
+    #[test]
+    fn verify_returns_true_when_passwords_match() {
+        let p = Password::try_from("password123").unwrap();
+        let hash = PasswordHash::try_from(&p).unwrap();
+
+        let verify = hash.verify(&p);
+        assert!(verify.is_ok());
+        let verify = verify.unwrap();
+        assert!(verify);
+    }
+
+    #[test]
+    fn verify_returns_false_when_passwords_do_not_match() {
+        let p = Password::try_from("password123").unwrap();
+        let hash = PasswordHash::try_from(&p).unwrap();
+
+        let p = Password::try_from("123password").unwrap();
+        let verify = hash.verify(&p);
+        assert!(verify.is_ok());
+        let verify = verify.unwrap();
+        assert!(!verify);
     }
 }
