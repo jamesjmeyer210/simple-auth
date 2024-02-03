0b878c62c3e7a5aa428b049e329f323139016ea0
James Meyer <jamesjmeyer210@gmail.com>
2024-02-03 09:07:46 -0700

    Added sign and verify functions

--- simple-auth-crypto/src/rsa/rsa_key_pair.rs
+++ simple-auth-crypto/src/rsa/rsa_key_pair.rs
@@ -2,12 +2,13 @@ use openssl::encrypt::Encrypter;
 use openssl::hash::MessageDigest;
 use openssl::pkey::{PKey, Private, Public};
 use openssl::rsa::{Padding, Rsa};
+use openssl::sign::{Signer, Verifier};
 use openssl::x509::{X509, X509Builder, X509NameBuilder};
 
 pub(crate) struct RsaKeyPair
 {
     public: Rsa<Public>,
-    private: Rsa<Private>
+    private_key: PKey<Private>
 }
 
 impl RsaKeyPair {
@@ -15,24 +16,35 @@ impl RsaKeyPair {
 
     pub fn generate() -> Self {
         let private = Rsa::generate(Self::PK_SIZE).unwrap();
-        let n = private.n().clone().to_owned().unwrap();
-        let e = private.e().clone().to_owned().unwrap();
+        let n = private.n().to_owned().unwrap();
+        let e = private.e().to_owned().unwrap();
 
         let public = Rsa::from_public_components(n, e).unwrap();
 
         Self {
             public,
-            private
+            private_key: PKey::from_rsa(private.clone()).unwrap()
         }
     }
 
-    pub fn public(&self) -> PKey<Public> {
+    fn public(&self) -> PKey<Public> {
         PKey::from_rsa(self.public.clone()).unwrap()
     }
 
+    pub fn sign(&self, data: &[u8]) -> Vec<u8> {
+        let mut signer = Signer::new(MessageDigest::sha256(), &self.private_key).unwrap();
+        signer.update(data).unwrap();
+        signer.sign_to_vec().unwrap()
+    }
+
+    pub fn verify_sig(&self, data: &[u8], sig: &[u8]) -> bool {
+        let mut verifier = Verifier::new(MessageDigest::sha256(), &self.private_key).unwrap();
+        verifier.update(data).unwrap();
+        verifier.verify(sig).unwrap()
+    }
+
     pub fn encrypt(&self, data: &[u8]) -> Vec<u8> {
-        let private = self.private();
-        let mut encrypter = Encrypter::new(&private).unwrap();
+        let mut encrypter = Encrypter::new(&self.private_key).unwrap();
         encrypter.set_rsa_padding(Padding::PKCS1).unwrap();
 
         let buffer_len = encrypter.encrypt_len(data).unwrap();
@@ -54,12 +66,8 @@ impl RsaKeyPair {
         builder.set_subject_name(&x509_name).unwrap();
 
         builder.set_pubkey(&self.public()).unwrap();
-        builder.sign(&self.private(), MessageDigest::sha256()).unwrap();
+        builder.sign(&self.private_key, MessageDigest::sha256()).unwrap();
         builder.build()
-    }
-
-    fn private(&self) -> PKey<Private> {
-        PKey::from_rsa(self.private.clone()).unwrap()
     }
 }
 
@@ -73,7 +81,7 @@ mod test {
     fn generate_returns_key_pair() {
         let pair = RsaKeyPair::generate();
 
-        assert_eq!(RsaKeyPair::PK_SIZE/8, pair.private.size());
+        assert_eq!((RsaKeyPair::PK_SIZE/8) as usize, pair.private_key.size());
 
         let pk = pair.public();
         assert!(pk.rsa().is_ok());
@@ -92,10 +100,18 @@ mod test {
         let t = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
         let t = t.as_millis();
 
-        let txt = x509.to_text().unwrap();
-        fs::write(format!("{}.txt", t), txt).unwrap();
-
         let pem = x509.to_pem().unwrap();
         fs::write(format!("{}.pem", t), pem).unwrap();
+    }
+
+    #[test]
+    fn sign_returns_vec() {
+        let msg = b"A message to be signed";
+
+        let pair = RsaKeyPair::generate();
+        let sig = pair.sign(msg);
+
+        assert!(sig.len() > 1);
+        assert!(pair.verify_sig(msg, sig.as_slice()));
     }
 }
