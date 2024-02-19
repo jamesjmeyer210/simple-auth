f7799e51d7fff173b6a73c259f3b466ac0d9e252
James Meyer <jamesjmeyer210@gmail.com>
2024-02-19 11:11:05 -0700

    Added a universal error type

---
+++ simple-auth-error/src/error_kind.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+pub enum ErrorKind {
+    Cancelled,
+    Unknown,
+    InvalidArgument,
+    DeadlineExceeded,
+    NotFound,
+    AlreadyExists,
+    PermissionDenied,
+    ResourceExhausted,
+    FailedPrecondition,
+    Aborted,
+    OutOfRange,
+    Unimplemented,
+    Internal,
+    Unavailable,
+    DataLoss,
+}
